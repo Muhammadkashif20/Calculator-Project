997d5baec7b5eee33433ea92e6010fef858a1dbe
muhammadkashif20 <kashif28@gmail.com>
2024-08-07 20:34:24 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Calculator-Project
+ I made This is Simple Calculator Project Using Languages Html , Css , JavaScript. With Fully Functionality With JavaScript  & Using Eval Function.Styling For Css & Structure For Html. With Beautiful Ui 
---
+++ app.js
@@ -0,0 +1,27 @@
+// Programming For Calculator Project:-
+let disValue=document.getElementById('dis-Value')
+function valueGet(value) {
+ disValue.value+=value
+}
+function delAll() {
+disValue.value=''
+}
+function delsequence() {
+    if(disValue.value){
+        disValue.value=disValue.value.slice(0,-1)
+    }
+}
+function valueResult() {
+    try{
+        if(disValue.value){
+            disValue.value=eval(disValue.value)
+        }
+        else{
+            disValue.value='Syntax Error'
+        }
+    }catch{
+        disValue.value = 'Syntax Error'
+    }
+}
+
+
